e1ccc9448edc32951b235c2e8d7b25e4c740f981
dahye <dahye9011@naver.com>
2026-01-04 15:57:02 +0900

    feat(Employee): boolean 메서드 추가

--- src/main/java/oncall/Employee.java
+++ src/main/java/oncall/Employee.java
@@ -2,9 +2,11 @@ package oncall;
 
 public class Employee {
     private final EmployeeName employeeName;
+    private final int workingDay;
 
-    public Employee(EmployeeName employeeName) {
+    public Employee(EmployeeName employeeName, int workingDay) {
         this.employeeName = employeeName;
+        this.workingDay = workingDay;
     }
 
     @Override
@@ -24,5 +26,10 @@ public class Employee {
     @Override
     public int hashCode() {
         return employeeName.hashCode();
+    }
+
+    // 내가 어제 일했는지 판단
+    public boolean isWorked(int day) {
+        return workingDay == day;
     }
 }
