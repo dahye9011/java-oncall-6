c002429e06f2bd924d6e374cb78310baf3c316e3
dahye <dahye9011@naver.com>
2026-01-04 15:51:08 +0900

    feat(Employee): 근무자 및 근무자 이름 도메인 구현

---
+++ src/main/java/oncall/Employee.java
@@ -0,0 +1,28 @@
+package oncall;
+
+public class Employee {
+    private final EmployeeName employeeName;
+
+    public Employee(EmployeeName employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Employee other = (Employee) obj;
+        return employeeName.equals(other.employeeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return employeeName.hashCode();
+    }
+}
---
+++ src/main/java/oncall/EmployeeName.java
@@ -0,0 +1,47 @@
+package oncall;
+
+public class EmployeeName {
+    private static final int MAX_NAME_SIZE = 5;
+    private final String value;
+
+    public EmployeeName(String value) {
+        validateEmployeeName(value);
+        this.value = value;
+    }
+
+    private void validateEmployeeName(String value) {
+        validateNotBlank(value);
+        validateNameLength(value);
+    }
+
+    private void validateNotBlank(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 근무자 이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(String value) {
+        if (value.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 이름 길이는 최대 5자까지 가능합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        EmployeeName other = (EmployeeName) obj;
+        return value.equals(other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
