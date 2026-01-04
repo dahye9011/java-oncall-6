16e686612fb690af82576303fc9c2fdb0ab44a96
dahye <dahye9011@naver.com>
2026-01-04 23:36:38 +0900

    feat(ScheduleMaker): 날짜만큼 이름 리스트 생성 메서드 구현

--- src/main/java/oncall/ScheduleMaker.java
+++ src/main/java/oncall/ScheduleMaker.java
@@ -2,21 +2,17 @@ package oncall;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ScheduleMaker {
-    public Schedules makeSchedules(Employees employees, int dayCount) {
+    // 1일부터 dayCount까지 돌면서, 매일 배정된 이름을 하나씩 만들기
+    public List<EmployeeName> makeScheduleForDayCount(Employees employees, int dayCount) {
         List<Employee> employeeList = employees.getEmployeeList();
-        List<Schedule> scheduleList = new ArrayList<>();
+        List<EmployeeName> employeeNameList = new ArrayList<>();
 
         for (int i = 0; i < dayCount; i++) {
-            Map<Integer, EmployeeName> map = employeeList.stream()
-                    .collect(Collectors.toMap(e -> e.getWorkingDay(), // 근무 날짜
-                            e -> e.getEmployeeName())); // 근무자 이름
-            scheduleList.add(new Schedule(map));
+            employeeNameList.add(employeeList.get(i).getEmployeeName());
         }
 
-        return new Schedules(scheduleList);
+        return employeeNameList;
     }
 }
